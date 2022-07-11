b27da269ad75e5f15f95590dd2e7a858dc033e2a
DyanaFurriel <dyanafurriel.dsf@gmail.com>
2022-07-11 11:33:39 -0300

    Desafio de Loop

---
+++ script.js
@@ -0,0 +1,26 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let i=1; i<=10;i++){
+console.log(i);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let amora =10
+while(1 <= amora){
+    console.log(amora)
+    amora -= 1
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+let luz =1
+while(100>= luz){
+    console.log(luz)
+    luz=luz+2
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+let amor =0
+while(100>=amor){
+    console.log(amor)
+    amor=amor+2
+}
+
